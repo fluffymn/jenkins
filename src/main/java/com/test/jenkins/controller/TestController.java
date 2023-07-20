e07b14294d517dc5701a6419c2bc615d955ebcd9
fluffymn <mineezm9@gmail.com>
2023-07-20 13:00:09 +0900

    add test controller

---
+++ src/main/java/com/test/jenkins/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.test.jenkins.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+	@GetMapping("/")
+	public ResponseEntity<String> index(){
+		return ResponseEntity.ok("test");
+	}
+
+}
